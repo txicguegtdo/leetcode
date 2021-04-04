51d1d2fa1e732a80e8bbb806b4656fe31031cb9f
txicguegtdo <tlw81158307921183@163.com>
2021-04-04 18:10:32 +0800

    leetcode 26 - remove duplicates from sorted array

---
+++ 026-remove-duplicates-from-sorted-array/1.js
@@ -0,0 +1,20 @@
+// 26. Remove Duplicates from Sorted Array
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  let i = 1;
+  let n = nums.length;
+  for (let j = 1; j < n; j++) {
+    if (nums[i - 1] !== nums[j]) {
+      nums[i++] = nums[j];
+    }
+  }
+  return i;
+};
+
+// let nums = [1, 1, 1, 2, 3, 3];
+// console.info(removeDuplicates(nums));
