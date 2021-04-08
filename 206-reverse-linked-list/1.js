77341f3f7804be76f625507ac8481bb7c996a055
txicguegtdo <tlw81158307921183@163.com>
2021-04-08 13:27:22 +0800

    leetcode 206 - reverse linked list

---
+++ 206-reverse-linked-list/1.js
@@ -0,0 +1,24 @@
+// 206. Reverse Linked List
+// https://leetcode.com/problems/reverse-linked-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  let pre = null, cur = head;
+  while (cur) {
+    let next = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = next;
+  }
+  return pre;
+};
