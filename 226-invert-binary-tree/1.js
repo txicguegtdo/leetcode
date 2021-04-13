8cb22282d0b8489cb3178583ac2890baa72c48d1
txicguegtdo <tlw81158307921183@163.com>
2021-04-13 14:43:15 +0800

    leetcode 226 - invert binary tree

---
+++ 226-invert-binary-tree/1.js
@@ -0,0 +1,23 @@
+// 226. Invert Binary Tree
+// https://leetcode.com/problems/invert-binary-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+  if (!root) return null;
+
+  const {left, right} = root;
+  root.left = invertTree(right);
+  root.right = invertTree(left);
+
+  return root;
+};
